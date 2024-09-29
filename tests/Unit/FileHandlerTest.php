1ab82b4a794431667722eabd3a6b110d9aa305c6
Thomas Laure <thomaslaure3@gmail.com>
2024-09-29 13:09:02 +0200

    test(FileHandler): read + write tests

--- tests/Unit/FileHandlerTest.php
+++ tests/Unit/FileHandlerTest.php
@@ -26,4 +26,37 @@ class FileHandlerTest extends TestCase
 
         $this->fileHandler->remove($filePath);
     }
+
+    public function testWrite(): void
+    {
+        $filePath = 'test-file-path';
+        $content = 'test-content';
+
+        $this->filesystem
+            ->expects($this->once())
+            ->method('appendToFile')
+            ->with($filePath, $content);
+
+        $this->fileHandler->write($filePath, $content);
+    }
+
+    public function testRead(): void
+    {
+        $testFile = sys_get_temp_dir().'/test-file.gz';
+        
+        $content = "line1\nline2\nline3\n";
+        file_put_contents('compress.zlib://'.$testFile, $content);
+
+        $lines = [];
+        foreach ($this->fileHandler->read($testFile) as $line) {
+            $lines[] = $line;
+        }
+
+        $this->assertCount(3, $lines);
+        $this->assertEquals('line1', trim($lines[0]));
+        $this->assertEquals('line2', trim($lines[1]));
+        $this->assertEquals('line3', trim($lines[2]));
+
+        unlink($testFile);
+    }
 }
